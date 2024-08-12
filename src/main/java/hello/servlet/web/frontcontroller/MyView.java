c4b4e81f8a329d553f946289a747c769b0a02662
sosa7753 <sosa7753@naver.com>
2024-08-12 23:25:37 +0900

    섹션 4

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -20,5 +21,18 @@ public class MyView {
 
     RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
     dispatcher.forward(request, response);
+  }
+
+  public void render(Map<String, Object> model,
+      HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    modelToRequestAttribute(model, request); // JSP는 getAttribute로 데이터를 가져옴.
+    RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+    dispatcher.forward(request, response);
+  }
+
+  private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+    model.forEach((key, value) -> request.setAttribute(key, value));
   }
 }
