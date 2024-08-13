91535dfcd04dedda33d181e249dcc8f840c656bc
sosa7753 <sosa7753@naver.com>
2024-08-13 15:05:56 +0900

    섹션 4

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -2,7 +2,6 @@ package hello.servlet.web.frontcontroller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
