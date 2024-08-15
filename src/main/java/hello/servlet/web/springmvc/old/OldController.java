a6f8352a7bfc7ec3c5fc47750b03bdf7046c7623
sosa7753 <sosa7753@naver.com>
2024-08-16 00:10:59 +0900

    섹션 5

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") // 스프링 빈 이름으로 핸들러를 찾는 매핑이있음.(우선순위 2위)
+public class OldController implements Controller {
+
+  @Override
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    System.out.println("OldController.handleRequest");
+    return null;
+  }
+}
